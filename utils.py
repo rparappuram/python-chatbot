97fc100e43b05056494554f1e72d423dd4463010
rparappuram <rparappuram@gmail.com>
2023-06-08 14:15:44 -0700

    fixed refined query to only look at prev message

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def query_refiner(conversation, query):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-        temperature=0.5,
+        temperature=0.1,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
@@ -25,13 +25,15 @@ def query_refiner(conversation, query):
     return response["choices"][0]["text"]
 
 
-# combine conversation history into a single string
-# TODO - add a "k" parameter to limit the number of messages to include in the conversation string
+# combine the last request and response into a single string
 def get_conversation_string():
     conversation_string = ""
-    for i in range(len(st.session_state["responses"]) - 1):
-        conversation_string += "Human: " + st.session_state["requests"][i] + "\n"
-        conversation_string += "Bot: " + st.session_state["responses"][i + 1] + "\n"
+    response_length = len(st.session_state["responses"])
+    request_length = len(st.session_state["requests"])
+    if len(st.session_state["requests"]) != 0:
+        conversation_string += "Human: " + st.session_state["requests"][request_length-1] + "\n"
+    if len(st.session_state["responses"]) != 0:
+        conversation_string += "Bot: " + st.session_state["responses"][response_length-1] + "\n"
     return conversation_string
 
 
